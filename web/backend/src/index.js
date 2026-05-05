dfe17079d03b726ae945e59e5411f1810e754f23
gaganalexa <202310537@fit.edu.ph>
2026-05-06 03:13:05 +0800

    fix: update CORS configuration to allow login credentials in mobile

--- web/backend/src/index.js
+++ web/backend/src/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
