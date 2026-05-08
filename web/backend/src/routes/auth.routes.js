d6c19e3993e4f7d584b5e2f1287346d973f3335d
mm-gonzales <michaela.v.gonzales@gmail.com>
2026-05-08 09:47:02 +0800

    refactor: rename and update the signup and login validation middleware and route

--- web/backend/src/routes/auth.routes.js
+++ web/backend/src/routes/auth.routes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/auth.controller');
-const { loginRules, signupRules, validateLogin } = require('../middleware/login_validation.middleware');
+const { loginRules, signupRules, validateLogin } = require('../middleware/auth_validation.middleware');
 
 router.post('/signup', signupRules, validateLogin, signup);
 router.post('/login', loginRules, validateLogin, login); 
